aad9c76986ccac147dbc7b636e818bb79e170675
Shiva4599 <84388223+Shiva4599@users.noreply.github.com>
2024-12-05 12:06:53 +0000

    Variables and datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const stdid= 184
+var stdmail="std@mail.com"
+let stdacc="23648"
+stdmarks=126354;
+var std 
+console.log(stdid) 
+
+console.table([stdmail,stdacc,stdmarks,std]);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,15 @@
+/*number=2 to power 53
+bigint
+String=""
+boolean=true/false
+null=standalone value
+undefined
+symbol=unique
+
+object
+
+ null is an object but undefined is a type
+*/
+
+console.log(typeof null)
+console.log(typeof undefined)
